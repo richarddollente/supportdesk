ee0b8392b9039f4c7af27ada09f6d569056872cf
richarddollente <dollente.richard@panpacificu.edu.ph>
2021-08-17 20:37:07 -0700

    update login.php with resources

--- login.php
+++ login.php
@@ -1,7 +1,31 @@
 <?php
-        include("resources/header.php");
-        require_once("resources/navigation.php");
-        
+    include("resources/header.php");
+    require_once("resources/navigation.php");
+    require_once("resources/classes/UserAccounts.php");
+    require_once("resources/classes/Constants.php");
+    require_once("resources/classes/FormSanitizer.php");
+
+    $useraccount = new UserAccounts($connection);
+
+    if(isset($_POST["loginSubmit"])) {
+
+        $userEmail = FormSanitizer::sanitizeFormUserEmail($_POST["userEmail"]);
+        $userPassword = FormSanitizer::sanitizerFormUserPassword($_POST["userPassword"]);
+
+        $isSuccessful = $useraccount->login($userEmail, $userPassword);
+
+        if($isSuccessful){
+            $_SESSION["userLoggedIn"] = $userEmail;
+            header("profile: login.php");
+        }
+    }
+
+    function getInputValue($user){
+        if(isset($_POST[$user])){
+            echo $_POST[$name];
+        }
+    }
+
 ?>
 <body>
     <div class="login-container">
@@ -13,12 +37,14 @@
             </div>
             <div class="loginForm">
                 <form action="login.php" method="POST">
+                    <?php echo $useraccount->getError(Constants::$loginFailed); ?>
                     <input type="text" name="userEmail" placeholder="Email" value="" required>
                     <input type="password" name="userPassword" placeholder="Password" required>
                     <input type="submit" name="loginSubmit" value="LOGIN">
                 </form>
             </div>
         <a class="registerMessage" href="register.php">Register</a>
+        <br>
         <a class="forgotPWMessage" href="forgot-password.php">Forgot Password</a>
         </div>
     </div>
